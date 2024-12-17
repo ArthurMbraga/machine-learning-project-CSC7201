48c77a2028ebfa4f5231f497f246f1a7014affb5
Arthur Martins Braga <arthur.martinsbraga@telecom-paris.fr>
2024-12-17 22:11:27 +0100

    Changes

--- preprocessing.py
+++ preprocessing.py
@@ -5,6 +5,7 @@ from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 
+
 def preprocessData(df_train, df_test):
 
     def addAvgLagColumns(X):
@@ -40,13 +41,24 @@ def preprocessData(df_train, df_test):
     # Normalize
     numeric_features = new_df_train.select_dtypes(include=[np.number]).columns
 
+    # Remove rows with missing values
+    # new_df_train.dropna(inplace=True)
+    # new_df_test.dropna(inplace=True)
+
     numeric_transformer = Pipeline(steps=[
-        ('imputer', SimpleImputer(strategy='constant')),
-        ('scaler', StandardScaler())])
+        ('imputer', SimpleImputer(strategy='constant', fill_value=0)),
+        # ('scaler', StandardScaler())
+    ])
 
     preprocessor = ColumnTransformer(
         transformers=[
             ('num', numeric_transformer, numeric_features)])
+
+    # Transform lag columns into sqrt lag columns
+    lag_cols = ['t1s0', 't2s0', 't3s0', 't0s1', 't0s2', 't0s3', 't0s0']
+    for col in lag_cols:
+        new_df_train[col] = np.sqrt(new_df_train[col])
+        new_df_test[col] = np.sqrt(new_df_test[col])
 
     new_df_train = preprocessor.fit_transform(new_df_train)
     new_df_test = preprocessor.transform(new_df_test)
@@ -54,5 +66,13 @@ def preprocessData(df_train, df_test):
     # Keep data in DataFrame format
     new_df_train = pd.DataFrame(new_df_train, columns=numeric_features)
     new_df_test = pd.DataFrame(new_df_test, columns=numeric_features)
+
+    # Put t0s0 as the last column
+    cols = new_df_train.columns.tolist()
+    cols.remove('t0s0')
+    cols.append('t0s0')
+
+    new_df_train = new_df_train[cols]
+    new_df_test = new_df_test[cols]
 
     return new_df_train, new_df_test
